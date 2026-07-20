2c0ed244f54427efcb58292998adb8ead6d9b60f
williamkenney101 <williamkenney101@gmail.com>
2026-07-20 11:25:30 -0400

    more hlepful comment

--- tools/util.py
+++ tools/util.py
@@ -3,6 +3,6 @@
 # Copyright 2024 The MITRE Corporation. ALL RIGHTS RESERVED
 # Approved for public release. Distribution unlimited 23-02181-25.
 
-def print_hex(data):
+def print_hex(data): # data to hex conversion for utility
     hex_string = " ".join(format(byte, "02x") for byte in data)
-    print(hex_string)
+    print(hex_string) # print string
